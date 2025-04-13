793a5d6e42dc4d7f46956f2d9c3abddb0d48fc59
smoriginals <contactsmoriginals@gmail.com>
2025-04-14 01:35:06 +0530

    update
    Setup Frontend in Progress...

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Cards from './components/Cards';
-import Newsitems from './components/Newsitems';
 import { Routes, Route } from 'react-router-dom';
 import React, {useState} from 'react';
 import LoadingBar from "react-top-loading-bar";
@@ -16,20 +15,14 @@ export default function App(props) {
     return (
         <>
             <LoadingBar
-                color="blue"
+                color="green"
                 progress={progress}
                 onLoaderFinished={()=>setProgress(0)}
             />
 
-            <Navbar title="BBC NEWS" />
+            <Navbar title="SMOS Gallery" />
             <Routes>
-                <Route excat path="/" element={<Newsitems setProgress={setProgress}  key="general" pageSize={6} country="us" endPoint="top-headlines" category="general" />} />
-                <Route excat path="/entertainment" element={<Newsitems setProgress={setProgress} key="entertainment" pageSize={6} country="us" endPoint="top-headlines" category="entertainment" />} />
-                <Route excat path="/health" element={<Newsitems setProgress={setProgress} key="health" pageSize={6} country="us" endPoint="top-headlines" category="health" />} />
-                <Route excat path="/science" element={<Newsitems setProgress={setProgress} key="science" pageSize={6} country="us" endPoint="top-headlines" category="science" />} />
-                <Route excat path="/technology" element={<Newsitems setProgress={setProgress} key="technology" pageSize={6} country="us" endPoint="top-headlines" category="technology" />} />
-                <Route excat path="/business" element={<Newsitems setProgress={setProgress} key="business" pageSize={6} country="us" endPoint="top-headlines" category="business" />} />
-                <Route excat path="/sports" element={<Newsitems setProgress={setProgress} key="sports" pageSize={6} country="us" endPoint="top-headlines" category="sports" />} />
+               <Route/>
             </Routes>
         </>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,80 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css'
 export default function Navbar(props) {
 
+    const [inputValue, setInputValue] = useState('');
+
+    const HandleChange = (e) => {
+        setInputValue(e.target.value);
+        console.log(e.target.value);
+        if (e.key === 'Enter') {
+            console.log('Enter key pressed');
+        }
+
+    }
+    const HandleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            if (inputValue.trim() !== '') {
+                HandleClick();
+            }
+        }
+    }
+    const HandleClick = (e) => {
+        if (inputValue.trim() === '') {
+            console.log('Empty');
+        }
+        else {
+            console.log(inputValue);
+            setInputValue('');
+        }
+    }
+    
     return (
         <>
-            <nav className="navbar navbar-expand-lg">
+            <nav className="navbar navbar-expand-lg backg">
                 <div className="container-fluid">
-                    <Link className="navbar-brand textlight">SMOS Gallery</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <Link className="navbar-brand ">{props.title}</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link textlg">Home</Link>
+                                <Link className="nav-link mx-2 backg" to='/'>Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link textlg" to="/entertainment">Entertainment</Link>
+                                <Link className="nav-link mx-1 backg" to="/nature">Nature</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link textlg" to="/health">Health</Link>
+                                <Link className="nav-link mx-1 backg" to="/business">Business</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link textlg" to="/sports">Sports</Link>
+                                <Link className="nav-link mx-1 backg" to="/sports">Sports</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link mx-1 backg" to="/cars">Cars</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link mx-1 backg" to="/sky">Sky's</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link mx-1 backg" to="/videos">Videos</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link mx-1 backg" to="/3d">3D</Link>
                             </li>
                         </ul>
+                       
                     </div>
                 </div>
             </nav>
             <div className='container'>
                 <div className="container-fluid mt-5 pt-4">
                     <div className="d-flex">
-                        <input className="form-control mx-2 br" placeholder='Search For Free Images...'/>
-                        <button className="btn btn-primary btn-anim br">Search</button>
+                        <input className="form-control mx-2 br" placeholder='Search For Free Images...' onChange={HandleChange} onKeyDown={HandleKeyPress} />
+                        <button className="btn btn-primary btn-anim br" onClick={HandleClick}>Search</button>
+                        
                     </div>
                 </div>
             </div>
